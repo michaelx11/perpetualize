d21654c57ab68a8a336391b59d4f597b712780e0
Michael Xu <michaeljxu11@gmail.com>
2015-04-23 15:06:21 -0400

    Initial Commit

---
+++ PerpetualizeV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerpetualizeV2
+//
+//  Created by Michael Xu on 4/23/15.
+//  Copyright (c) 2015 Michael Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PerpetualizeV2Tests/PerpetualizeV2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PerpetualizeV2Tests.swift
+//  PerpetualizeV2Tests
+//
+//  Created by Michael Xu on 4/23/15.
+//  Copyright (c) 2015 Michael Xu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PerpetualizeV2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
